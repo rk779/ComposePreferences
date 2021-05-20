ed483f7e0032a6d470997bc8d04ac180ee89490c
Rahul Krishna <Rahulkrishna585@gmail.com>
2021-05-20 22:48:28 +0530

    app: Update sample application
    Signed-off-by: Rahul Krishna <Rahulkrishna585@gmail.com>

--- app/src/main/java/de/schnettler/composepreferences/MainActivity.kt
+++ app/src/main/java/de/schnettler/composepreferences/MainActivity.kt
@@ -1,9 +1,9 @@
 package de.schnettler.composepreferences
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
@@ -12,130 +12,140 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Warning
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import de.schnettler.composepreferences.ui.ComposePreferencesTheme
-import de.schnettler.datastore.compose.model.Preference
 import de.schnettler.datastore.compose.model.Preference.PreferenceGroup
 import de.schnettler.datastore.compose.model.Preference.PreferenceItem
 import de.schnettler.datastore.compose.ui.PreferenceScreen
+import de.schnettler.datastore.manager.DataStoreManager
 import kotlin.math.roundToInt
 
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
-
 class MainActivity : AppCompatActivity() {
-    @OptIn(ExperimentalMaterialApi::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val localDataStore = this.dataStore
+        setContent {
+            ComposePreferencesTheme {
+                AppPreferenceScreen()
+            }
+        }
+    }
+}
 
-        val listGroup = PreferenceGroup(
-            "List Group", false, listOf(
-                PreferenceItem.ListPreference(
-                    ListPrefExample,
-                    title = "List Preference",
-                    summary = "Select one item from a list in a dialog",
-                    singleLineTitle = true,
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Outlined.Warning,
-                            contentDescription = null,
-                            modifier = Modifier.padding(8.dp)
-                        )
-                    },
-                    entries = mapOf(
-                        "key1" to "Item1",
-                        "key2" to "Item2"
-                    ),
-                ),
-                PreferenceItem.MultiSelectListPreference(
-                    MultiPrefExample,
-                    title = "MultiSelect List Preference",
-                    summary = "Select multiple items from a list in a dialog",
-                    singleLineTitle = true,
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Outlined.Warning,
-                            contentDescription = null,
-                            modifier = Modifier.padding(8.dp)
-                        )
-                    },
-                    entries = mapOf(
-                        "key1" to "Item1",
-                        "key2" to "Item2"
-                    ),
-                ),
-                PreferenceItem.DropDownMenuPreference(
-                    DropDownPrefExample,
-                    title = "DropDown Menu Preference",
-                    summary = "Select an item from a dropdown menu",
-                    singleLineTitle = true,
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Outlined.Warning,
-                            contentDescription = null,
-                            modifier = Modifier.padding(8.dp)
-                        )
-                    },
-                    entries = mapOf(
-                        "key1" to "Item1",
-                        "key2" to "Item2"
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun AppPreferenceScreen() {
+    val dataStore = LocalContext.current.dataStore
+    val dataStoreManager = remember { DataStoreManager(dataStore) }
+    val switchPreference = dataStoreManager.getPreferenceFlow(SwitchPrefExample)
+        .collectAsState(initial = false)
+
+    val listGroup = PreferenceGroup(
+        title = "List Group",
+        enabled = switchPreference.value,
+        preferenceItems = listOf(
+            PreferenceItem.ListPreference(
+                request = ListPrefExample,
+                title = "List Preference",
+                summary = "Select one item from a list in a dialog",
+                singleLineTitle = true,
+                icon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Warning,
+                        contentDescription = null,
+                        modifier = Modifier.padding(8.dp)
                     )
+                },
+                entries = mapOf(
+                    "key1" to "Item1",
+                    "key2" to "Item2"
+                )
+            ),
+            PreferenceItem.MultiSelectListPreference(
+                request = MultiPrefExample,
+                title = "MultiSelect List Preference",
+                summary = "Select multiple items from a list in a dialog",
+                singleLineTitle = true,
+                icon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Warning,
+                        contentDescription = null,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                },
+                entries = mapOf(
+                    "key1" to "Item1",
+                    "key2" to "Item2"
+                )
+            ),
+            PreferenceItem.DropDownMenuPreference(
+                request = DropDownPrefExample,
+                title = "DropDown Menu Preference",
+                summary = "Select an item from a dropdown menu",
+                singleLineTitle = true,
+                icon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Warning,
+                        contentDescription = null,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                },
+                entries = mapOf(
+                    "key1" to "Item1",
+                    "key2" to "Item2"
                 )
             )
         )
+    )
 
-        setContent {
-            ComposePreferencesTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = { Text(text = "Compose Preferences") }
-                        )
-                    },
-                    content = {
-                        PreferenceScreen(
-                            items = listOf(
-                                PreferenceItem.SwitchPreference(
-                                    SwitchPrefExample,
-                                    title = "Switch Preference",
-                                    summary = "A preference with a switch.",
-                                    singleLineTitle = true,
-                                    icon = {
-                                        Icon(
-                                            imageVector = Icons.Outlined.Warning,
-                                            contentDescription = null,
-                                            modifier = Modifier.padding(8.dp)
-                                        )
-                                    }
-                                ),
-                                listGroup,
-                                PreferenceItem.SeekBarPreference(
-                                    SeekPrefExample,
-                                    title = "Seekbar Preference",
-                                    summary = "Select a value on a seekbar",
-                                    singleLineTitle = true,
-                                    icon = {
-                                        Icon(
-                                            imageVector = Icons.Outlined.Warning,
-                                            contentDescription = null,
-                                            modifier = Modifier.padding(8.dp)
-                                        )
-                                    },
-                                    steps = 4,
-                                    valueRange = 50F..100F,
-                                    valueRepresentation = { value -> "${value.roundToInt()} %" }
-                                )
-                            ),
-                            dataStore = localDataStore
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Compose Preferences") }
+            )
+        },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        PreferenceScreen(
+            items = listOf(
+                PreferenceItem.SwitchPreference(
+                    request = SwitchPrefExample,
+                    title = "Switch Preference",
+                    summary = "A preference with a switch.",
+                    singleLineTitle = true,
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Warning,
+                            contentDescription = null,
+                            modifier = Modifier.padding(8.dp)
                         )
                     }
-                )
-            }
-        }
+                ),
+                PreferenceItem.SeekBarPreference(
+                    request = SeekPrefExample,
+                    title = "Seekbar Preference",
+                    summary = "Select a value on a seekbar",
+                    singleLineTitle = true,
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Warning,
+                            contentDescription = null,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    },
+                    steps = 4,
+                    valueRange = 50F..100F,
+                    valueRepresentation = { value -> "${value.roundToInt()} %" }
+                ),
+                listGroup
+            ),
+            dataStore = dataStore
+        )
     }
 }
--- app/src/main/java/de/schnettler/composepreferences/Settings.kt
+++ app/src/main/java/de/schnettler/composepreferences/Settings.kt
@@ -1,7 +1,16 @@
 package de.schnettler.composepreferences
 
-import androidx.datastore.preferences.core.*
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.floatPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.stringSetPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import de.schnettler.datastore.manager.PreferenceRequest
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 object SwitchPrefExample : PreferenceRequest<Boolean>(booleanPreferencesKey("pref_switch"), true)
 object ListPrefExample: PreferenceRequest<String>(stringPreferencesKey("pref_list"), "")
